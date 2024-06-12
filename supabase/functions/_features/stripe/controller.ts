c3cb069ab08d35e91818f2ba904910a60530a929
Ric <ricjohn.genoguin@gmail.com>
2024-06-12 22:37:19 +1200

    refactor: qol life fixes

--- supabase/functions/_features/stripe/controller.ts
+++ supabase/functions/_features/stripe/controller.ts
@@ -99,9 +99,10 @@ export const createOrUpdateSubscription = async (
             {
               id: existingSubscription.items.data[0].id,
               price: priceId,
-              quantity: existingSubscription.items.data[0].quantity + quantity,
+              quantity,
             },
           ],
+          metadata: { seats },
         },
       );
 
